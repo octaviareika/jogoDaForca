53b382fddc8bd49e628272ca228f0deada09c501
octaviareika <ceciliaperet81@gmail.com>
2024-01-04 15:10:53 -0300

    iniciando jogo da forca

---
+++ index.js
@@ -0,0 +1,11 @@
+// criar um jogo da forca
+
+let opcao = 0;
+
+console.log("Bem vindo ao jogo da forca!\n");
+console.log("Escolha uma opção:\n");
+console.log("1 - Jogar\n");
+console.log("2 - Sobre\n");
+console.log("3 - Sair\n");
+const prompt = require("prompt-sync")(); // instancar o prompt-sync
+opcao = prompt();
